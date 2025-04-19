1fdd72c152320918749a4238231cc4fce7439c67
unknown <vi-del>
2025-04-19 14:27:45 +0700

    'Push'

---
+++ TestDB/SinhVien_HienThi.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using FontAwesome.Sharp;
+using Oracle.ManagedDataAccess.Client;
+
+
+namespace TestDB
+{
+    public partial class SinhVien_HienThi : Form
+    {
+        public SinhVien_HienThi()
+        {
+            InitializeComponent();
+        }
+
+        private void SinhVien_HienThi_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
